1c3b6b0cd0be0c9a71a60e261af196ac7abdc752
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2023-01-19 14:38:41 +0100

    Fix minimum deployment target

--- Tests/SpideyTests/SpideyTests.swift
+++ Tests/SpideyTests/SpideyTests.swift
@@ -6,6 +6,5 @@ final class SpideyTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Spidey().text, "Hello, World!")
     }
 }
